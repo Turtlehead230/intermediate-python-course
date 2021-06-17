165f263b56804af68122eaccc4d3f6ae3f7f7f80
Theo C <theocamargo23@gmail.com>
2021-06-17 17:40:23 -0300

    Meh

--- dice_roller.py
+++ dice_roller.py
@@ -2,13 +2,21 @@ import random
 
 
 def main():
-    dice_rolls = 2
+    dice_rolls = int(input('How many dice you wanna roll??'))
+    dice_sides = int(input('how many sides does you dice have??'))
     dice_sum = 0
     for i in range(0, dice_rolls):
-      roll = random.randint(1, 6)
+      roll = random.randint(1, dice_sides)
       dice_sum += roll
 
-    print(f'You rolled a {dice_sum}')
+    if roll == 1:
+      print(f'You rolled a {roll}, EPIC FAIL')
+    elif roll == dice_sides:
+      print(f'You rolled a {roll}, EPIC GAMER MOMENT')
+    else:
+      print(f'you rolled a {roll}')
+
+    print(f'You rolled a total of {dice_sum} !')
 
 if __name__== "__main__":
   main()
